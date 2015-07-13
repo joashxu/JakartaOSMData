5df4d069d5327dd624b6b513d54c7b8205512d27
joash xu <joash.xu@gmail.com>
2015-07-13 21:52:35 +0700

    Add similiar address name.

--- osm_datadescriptor.py
+++ osm_datadescriptor.py
@@ -3,6 +3,9 @@
 
 import xml.etree.cElementTree as ET
 import re
+import json
+import itertools
+from difflib import SequenceMatcher
 
 
 class OSMDataDescriptor(object):
@@ -79,3 +82,38 @@ class OSMDataDescriptor(object):
                         faulty_alley_prefixes.append(el.attrib['v'])
 
         return faulty_alley_prefixes
+
+    def audit_address_similar_names(self, reference_filename='street_name.json'):
+        '''Check if the street name match reference from wikipedia.
+
+        The reference is taken from Wikipedia info box.
+        Use difflib.SequenceMatcher to check how closely the name in the tag match the reference.
+        '''
+        # Load wikipedia street name data
+        street_names_file = open(reference_filename)
+        street_names = json.load(street_names_file)
+
+        # Flatten the street names, ignore the city
+        flatten_street_names = [item.lower() for item in itertools.chain(*street_names.values())]
+
+        closely_matched_names = set()
+        for event, element in ET.iterparse(self.filename, events=('start',)):
+            # Only pay attention to 'way' tag
+            if not element.tag == 'way':
+                continue
+
+            for el in element.iter('tag'):
+                # We only audit addr:street for this.
+                if el.attrib['k'] == 'addr:street':
+                    street_name = el.attrib['v'].lower()
+                    for ref_street_name in flatten_street_names:
+                        s = SequenceMatcher(lambda x: x == " ", street_name, ref_street_name)
+
+                        # Check the ratio between the two stings.
+                        # 0.6 is considered a close match,
+                        # 1 means the two strings match exactly.
+                        # We want ratio that is between the two.
+                        if 0.65 <= s.ratio() < 1.0:
+                            closely_matched_names.add((ref_street_name, street_name, s.ratio()))
+
+        return closely_matched_names
